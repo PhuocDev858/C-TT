1d0beb45a71538081682dca5ba7a2c734cb13dc8
Biriii <bangeabar@gmail.com>
2025-10-04 20:25:23 +0700

    FE
    ww

--- BE-QLXM/database/factories/UserFactory.php
+++ BE-QLXM/database/factories/UserFactory.php
@@ -23,9 +23,10 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        static $counter = 1;
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'User ' . $counter,
+            'email' => 'user' . $counter++ . '@example.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
@@ -37,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
--- BE-QLXM/routes/web.php
+++ BE-QLXM/routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test-db', function () {
-    try {
-        DB::connection()->getPdo();
-        return "✅ Kết nối thành công tới SQL Server!";
-    } catch (\Exception $e) {
-        return "❌ Lỗi: " . $e->getMessage();
-    }
+Route::get('/', function () {
+    return view('welcome');
 });
